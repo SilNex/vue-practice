8347820cacbc5a28a5f84eed90a0dca8b8244f79
silnex <silnex@silnex.kr>
2019-05-03 17:36:35 +0900

    Add mixin for practice

---
+++ vue-cli-mixin/src/components/UserDetail.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="red lighten-3 pa-3">
+    <h3>자세한 회원 정보를 확인합니다.</h3>
+    <p>상세사항</p>
+    <v-list dense>
+      <v-list-tile>
+        <v-list-tile-content>이름:</v-list-tile-content>
+        <v-list-tile-content class="align-end">
+          {{ name }}
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>주소:</v-list-tile-content>
+        <v-list-tile-content class="align-end">
+          {{ address }}
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>전화번호:</v-list-tile-content>
+        <v-list-tile-content class="align-end">
+          {{ phone }}
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>반려견 유무:</v-list-tile-content>
+        <v-list-tile-content class="align-end">
+          {{ hasDogKr }}
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>수정일자</v-list-tile-content>
+        <v-list-tile-content class="align-end">
+          {{ editedDate }}
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import { eventBus } from '../main'
+export default {
+  data () {
+    return {
+      eventBus: null
+    }
+  },
+  props: ['name', 'address', 'phone', 'hasDog'],
+  data () {
+    return {
+      editedDate: new Date()
+    }
+  },
+  computed: {
+    hasDogKr () {
+      return this.hasDog === true ? '있음' : '없음'
+    }
+  },
+  created () {
+    eventBus.$on('userWasEdited', (date) => {
+        this.editedDate = date
+    })
+  }
+}
+</script>
