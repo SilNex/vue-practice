f323d46ed0700d7c5d329fc9a2157afccc5e6406
silnex <silnex@silnex.kr>
2019-05-09 10:44:07 +0900

    Add login logic with vuex

--- vue-login-project/src/store.js
+++ vue-login-project/src/store.js
@@ -4,7 +4,37 @@ import Vuex from "vuex"
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    allUsers: [
+      { id: 1, name: "silnex", email: "silnex@silnex.kr", password: "1234" },
+      { id: 2, name: "noone", email: "noone@silnex.kr", password: "1234" }
+    ],
+    isLogin: false,
+    isLoginError: false
+  },
+  mutations: {
+    loginSuccess(state) {
+      state.isLogin = true
+      state.isLoginError = false
+    },
+    loginError(state) {
+      state.isLogin = false
+      state.isLoginError = true
+    }
+  },
+  actions: {
+    login({ state, commit }, loginObj) {
+      let selectedUser = null
+      state.allUsers.forEach(user => {
+        if (user.email === loginObj.email) {
+          selectedUser = user
+        }
+      })
+      selectedUser === null
+        ? commit("loginError")
+        : selectedUser.password !== loginObj.password
+        ? commit("loginError")
+        : commit("loginSuccess")
+    }
+  }
 })
