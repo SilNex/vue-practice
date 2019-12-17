a0fab23a28e1df60f73003149e4062112d65dbae
silnex <silnex@silnex.kr>
2019-12-17 21:37:24 +0900

    Remove old

--- old/vue-router-children/src/views/UsersEdit.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Users Edit</h1>
-    </div>
-</template>
--- old/vuex/src/views/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <v-container>
-    <h1>Home</h1>
-    <h4>모든 유저의 수: ({{ $store.getters.allUsersCount }})</h4>
-    <h1>서울 유저의 수: ({{ $store.getters.countOfSeoul }})({{ $store.getters.percentOfSeoul }}%)</h1>
-  </v-container>
-</template>
